b488bc7ac440cca9629d6018ee2dba19869d13d4
Caner Tangüler <tangulercaner@gmail.com>
2021-08-08 20:02:54 +0300

    ios vector icons setup

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://omgvamp-hearthstone-v1.p.rapidapi.com"
+export const BASE_URL = 'https://omgvamp-hearthstone-v1.p.rapidapi.com';
 
-export const API_KEY = "cea344db47msh1242ca1d7864614p1f7b3cjsn3b8f0d477df522"
+export const API_KEY = 'cea344db47msh1242ca1d7864614p1f7b3cjsn3b8f0d477df5';
 
-export const HOST = "omgvamp-hearthstone-v1.p.rapidapi.com"
+export const HOST = 'omgvamp-hearthstone-v1.p.rapidapi.com';
